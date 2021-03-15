bb2741faa63b320ba51d4829b831c0280ee6636c
kolinfox <df30650119@gmail.com>
2021-03-15 15:38:20 +0800

    2021.3.5

--- CCU_Project.py
+++ CCU_Project.py
@@ -1,8 +1,2 @@
-#https://www.youtube.com/watch?v=FyXlvui--Vw&t=348s&ab_channel=%E6%9D%8E%E6%94%BF%E8%BB%92
-#我的第一支快速利用TensorFlow 2.0建立CNN進行手寫數字分類
-#date 2021.3.5
 
-#tensorflow version 2.4.1
-
-import tensorflow as tf
 
