262dbda2c1f81ff432389d056e2a459abcb0349e
kolinfox <df30650119@gmail.com>
2021-03-15 17:29:20 +0800

    2021.3.15 1729

--- Practice/CNN_hand_writing.py
+++ Practice/CNN_hand_writing.py
@@ -35,6 +35,26 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 CNN = keras.Sequential(name = "CNN")
-CNN.add(layers.Conv2D(32, (3, 3), activation = "relu", input_shape = (28, 28 ,1))) # 2D的影像，32是convolution完後要有幾個node，(3, 3)是convolution的size，中間層儘量都用relu，第一層一定要有input_shpae
+#1層
+CNN.add(layers.Conv2D(32, (3, 3), activation = "relu", input_shape = (28, 28 ,1))) # 2D的影像，32是convolution完後要有幾個node，(3, 3)是convolution的size，中間層儘量都用relu，第一層一定要有input_shpae。Output會變成26*26，影像處理的概念。
+CNN.add(layers.MaxPooling2D(2, 2)) # 通常會搭配Maxpooling 2*2是習慣 13*13*32
+#2層
+CNN.add(layers.Conv2D(64, (3, 3), activation = "relu")) # 11*11*64
+CNN.add(layers.MaxPooling2D(2, 2)) # 通常會搭配Maxpooling 2*2是習慣 5*5*64
 
-keras.utils.plot_model(CNN ,to_file='./Practice/model.png' ,show_shapes=True) # 使用Graphviz顯示layers，會將結果儲存在to_file的路徑裡
+CNN.add(layers.Flatten()) # 壓平，5*5*64 = 1600 (64個5*5的圖像)。
+CNN.add(layers.Dense(128, activation = "relu"))
+CNN.add(layers.Dense(64, activation = "relu"))
+CNN.add(layers.Dense(10, activation = "softmax")) # 手寫資料集，0~9共10個。分類用softmax
+
+keras.utils.plot_model(CNN ,to_file='./Practice/model.png' ,show_shapes=True) # 使用Graphviz顯示layers，會將結果儲存在to_file的路徑裡。
+
+
+#編譯
+CNN.compile(optimizer = "Adam",
+            loss = keras.losses.sparse_categorical_crossentropy,
+            metrics = ["accuracy"])
+
+
+#開始比對
+CNN.fit(x_train, y_train, epochs = 5) # epochs可以用很多 但可能會overfitting
